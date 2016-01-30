0dd6960a43ea8838aed9bb1a2a069c408b1ff767
nasa9084 <nasa9084@live.jp>
2016-01-30 19:54:32 +0900

    split definition of item class into item.js

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-var Item = Class.create({
-    initialize: function(level, core_ref, map_ref, robo_ref){
-        this.level = level;
-
-        this.sprite = new Sprite(32, 32);
-        this.sprite.image = core_ref.assets["./resources/battery" + level + ".png"];
-        this.setPositionRandomly(map_ref);
-        core_ref.rootScene.addChild(this.sprite);
-        this.sprite.addEventListener("enterframe", function(){
-            if(this.frame == 0){
-                if(this.intersect(robo_ref.sprite)){
-                    this.frame -= 1;
-                    console.log(robo_ref.sprite.left);
-                    robo_ref.sprite.left += level*4;
-
-                    console.log(robo_ref.sprite.left);
-                }
-            }
-        })
-    },
-
-    setPositionRandomly: function(map){
-        var isPlased = false;
-        while (!isPlased) {
-            this.sprite.x = CELL_LENGTH * Math.floor(Math.random()*16);
-            this.sprite.y = CELL_LENGTH * Math.floor(Math.random()*16);
-            if (map.collisionData[this.sprite.y/CELL_LENGTH][this.sprite.x/CELL_LENGTH] == 0) {
-                isPlased = true;
-            }
-        }
-    }
-});
-
 var Robo = Class.create({
     initialize: function(initialBattery, initialPositionX, initialPositionY, core_ref, map_ref){
         this.sprite = new Sprite(32, 32);
